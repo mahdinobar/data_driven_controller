f8d9a97788045d92e53114efcbf51fd2735a9afe
mnobar <mahdi.nobar@idiap.ch>
2021-06-20 14:34:02 +0200

    demo 14_4

--- rup_kel_1.m
+++ rup_kel_1.m
@@ -2,10 +2,10 @@ function rup_kel_1
 clear all; clc; close all;
 
 % hyper-params
-idName= '14_2';
-N0=10;
+idName= '14_4';
+N0=3;
 N_iter=50;
-withSurrogate=false;
+withSurrogate=true;
 N_surrogate_repeat=10;
 Nsample=10;
 np2=2;
