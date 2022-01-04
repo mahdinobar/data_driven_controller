662ccdcbaf6f7c54dee1ed4f3a8ebe9ce57c7bbd
Mahdi <44223239+mahdinobar@users.noreply.github.com>
2022-01-04 22:55:50 +0100

    LVGBO, unstable

---
+++ LabView_GBO.m
@@ -0,0 +1,166 @@
+% BO
+global G2data
+addpath("C:\Users\nobar\Documents\LabVIEW Data\functions")
+addpath C:\Program Files\MATLAB\R2021b\toolbox\ident\ident\@iddata\iddata.m
+dir0="C:\Users\nobar\Documents\LabVIEW Data\N0_Data\";
+tmp_dir="C:\Users\nobar\Documents\LabVIEW Data\BO_Data\";
+idName= 'demo_BO_0_3';
+dir=append(tmp_dir,'/', idName, '/');
+if not(isfolder(dir))
+    mkdir(dir)
+end
+start_switch=1;
+withSurrogate=false;
+
+N0=1; %for N0>1 modify
+N_iter=50;
+N_iter=N_iter+N0;
+
+step_low=80;
+step_high=100;
+step_time=4;
+nr_repeats=2;
+control_mode=1;
+Input_mode=2;
+
+gain_angle=0;
+Tn_Angle=0;
+
+%% load gain limits
+dir_gains=append('C:\Users\nobar\Documents\data_driven_controller-main\data_driven_controller-main\tmp\DC_motor_gain_bounds\KpKi_bounds.mat');
+load(dir_gains)
+
+%% We define the function we would like to optimize
+%     fun = @(X)ObjFun_Guided(X, G, sampleTf, sampleTs, npG2, N_G, N_G2_activated, N_perturbed);
+fun = @(perf_Data) ObjFun(perf_Data); % CBO needs a function handle whose sole parameter is a vector of the parameters to optimize over.
+
+
+%% Setup the Gaussian Process (GP) Library
+addpath("C:\Users\nobar\Documents\data_driven_controller-main\data_driven_controller-main\gpml")
+startup;
+% Setting parameters for Bayesian Global Optimization
+opt.hyp = -1; % Set hyperparameters using MLE.
+opt.dims = 2; % Number of parameters.
+opt.mins = [Kp_min, Ki_min]; % Minimum value for each of the parameters. Should be 1-by-opt.dims
+opt.maxes = [Kp_max, Ki_max]; % Vector of maximum values for each parameter.
+opt.grid_size = 20000;
+opt.lt_const = 0.0;
+opt.do_cbo = 0; % Do CBO -- use the constraint output from F as well.
+opt.save_trace = 0;
+opt.trace_file=append(dir,'trace_file.mat');
+opt.resume_trace=true;
+% priors
+opt.meanfunc={@meanConst};
+opt.covfunc={@covMaternard, 5};
+% liklihood
+likfunc={@likGauss};
+% inference method
+infer=@infExact;
+
+%%
+% load initial dataset
+if counter==1
+    load(append(dir0, 'botrace0.mat'));
+    load(append(dir0, 'G2data_init.mat'));
+    G2data=G2data_init;
+    save(append(dir, 'G2data.mat'),'G2data')
+    if LVswitch==0 %for code consistency
+        load(append(dir0, 'perf_Data.mat'));
+    end
+    opt.resume_trace_data = botrace0;
+    clear botrace0
+elseif counter>1
+    load(append(dir, 'trace_file'))
+    opt.resume_trace_data = Trace;
+    clear Trace
+end
+
+opt.max_iters = size(opt.resume_trace_data.samples,1)+1;
+addpath("C:\Users\nobar\Documents\data_driven_controller-main\data_driven_controller-main")
+
+[ms,mv,Trace_tmp, LVgains] = bayesoptGPML(fun,opt,N0, LVswitch, perf_Data(end,:));
+
+%     LVswitch==0 means we need to call the system to get data
+if LVswitch==0
+    if counter==1 
+        G2=tfest(G2data, npG2);
+        C=tf([LVgains(1), LVgains(1)*LVgains(2)], [1, 0]);
+        CL=((step_high+step_low)/2)*feedback(C*G2, 1);
+        ov=abs(stepinfo(CL).Overshoot);
+        st=stepinfo(CL).SettlingTime;
+        [y,t]=step(CL);
+        reference=1;
+        e=abs(y-reference);
+        Tr=stepinfo(CL, 'RiseTimeLimits',[0.1,0.6]).RiseTime;
+        ITAE = trapz(t, t.*abs(e));
+        perf_Data=[perf_Data;[ov, Tr, st, ITAE]];
+        save(append(dir, 'G2_',num2str(counter)), 'G2')
+        LVswitch=0;
+    elseif idx==N_G
+        load(append(dir, 'G2data.mat'))
+        G2=tfest(G2data, npG2);
+        C=tf([LVgains(1), LVgains(1)*LVgains(2)], [1, 0]);
+        CL=((step_high+step_low)/2)*feedback(C*G2, 1);
+        ov=abs(stepinfo(CL).Overshoot);
+        st=stepinfo(CL).SettlingTime;
+        [y,t]=step(CL);
+        reference=1;
+        e=abs(y-reference);
+        Tr=stepinfo(CL, 'RiseTimeLimits',[0.1,0.6]).RiseTime;
+        ITAE = trapz(t, t.*abs(e));
+        perf_Data=[perf_Data;[ov, Tr, st, ITAE]];
+        idx= 0;
+        % remove previous G2
+        idx_G2= size(Trace_tmp.samples,1)-N_G;
+        Trace_tmp.samples(idx_G2,:)=[];
+        Trace_tmp.values(idx_G2)=[];
+        Trace_tmp.post_mus(idx_G2)=[];
+        Trace_tmp.post_sigma2s(idx_G2)=[];
+        Trace_tmp.times(idx_G2)=[];
+
+        save(append(dir, 'G2_',num2str(counter)), 'G2')
+        LVswitch=0;
+    else
+        ytmp = exp_Data(:,3);
+        utmp= exp_Data(:,4);
+        load(append(dir, 'G2data.mat'))
+        G2data = merge(G2data, iddata(ytmp,utmp,sampleTs));
+        save(append(dir, 'G2data.mat'),'G2data')
+        if idx==N_G
+            N_G2_activated_counter=N_G2_activated_counter+1;
+            idx=0;
+        else
+            idx= idx +1;
+        end
+
+        Kp=LVgains(1);
+        Ki=LVgains(2);
+        gain_vel=Kp;
+        Tn_vel=1/Ki;
+        LVswitch=LVswitch+1;
+        return
+    end
+elseif LVswitch==1
+    LVswitch=0;
+end
+
+Trace(1)=Trace_tmp;
+save(append(dir, 'trace_file.mat'),'Trace')
+save(append(dir, 'perf_Data_',num2str(counter)), 'perf_Data')
+save(append(dir, 'exp_Data_',num2str(counter)), 'exp_Data')
+counter=counter+1;
+
+return
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+gains0=[0.0950, 1.3293];
+Kp=gains0(1);
+Ki=gains0(2);
+gain_vel=Kp;
+Tn_vel=1/Ki;
+stat_value=0.5;
+Input_mode=0;
+LVswitch=0;
+counter=0;
+return
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
