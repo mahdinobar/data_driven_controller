1cc10c60db3f0a76a97a207fffd9148cd64511d7
mnobar <nobar.mahdi@gmail.com>
2023-06-05 21:45:54 +0200

    feasible set plot

--- LM_feasible_set.m
+++ LM_feasible_set.m
@@ -1,5 +1,6 @@
 %% Connect to OPCUA
-clear all; clc;
+clear all; clc;close all
+%%
 addpath('C:\Users\nobar\data_driven_controller\linear_motor')
 ip='192.168.188.21';
 sInfo = opcuaserverinfo(ip); % SPS Name
@@ -18,58 +19,60 @@ write_OPCUA(uaObj,'arrDemPos', r);
 write_OPCUA(uaObj,'arrShowPos', r);
 pause(1);
 %%
+s = tf('s');
 Kp = 0.44262;
 Tp = 0.070983;
 Td = 0.001;
 Gp_x = Kp/(1 * Tp *s) * 1/s * exp(-Td*s);
-F=0.0002;
-
+F=0.001;
+%%
+load('/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/exp_data_feasible.mat','exp_data')
 actPos_all=[];
 actVel_all=[];
 actCur_all=[];
 r_all=[];
 t_all=[];
-for D=10:1:90
-    for P=500:100:18000
-        disp(exe)
+for D=10:10:90
+    for P=500:500:12000
         C=P+D*s/(F*s+1);
         CLsys = feedback(Gp_x*C,1);
         [Gm,Pm,Wcg,Wcp] = margin(CLsys);
         if Pm>20
             % Set P and D gain
-            write_OPCUA(uaObj,'LQR_P_x', P);
-            write_OPCUA(uaObj,'LQR_D_v', D);
-            pause(0.2);
-
-            % perform experiment
-            write_OPCUA(uaObj,'Go', 1); % Perform Experiment
-            pause(7.5);
-
-            % read the results
-            actPos = read_OPCUA(uaObj,'arrActPos')';
-            actVel = read_OPCUA(uaObj,'arrActVel')';
-            actCur = read_OPCUA(uaObj,'arrActCur')';
-
+%             write_OPCUA(uaObj,'LQR_P_x', P);
+%             write_OPCUA(uaObj,'LQR_D_v', D);
+%             pause(0.2);
+% 
+%             % perform experiment
+%             write_OPCUA(uaObj,'Go', 1); % Perform Experiment
+%             pause(7.5);
+% 
+%             % read the results
+%             actPos = read_OPCUA(uaObj,'arrActPos')';
+%             actVel = read_OPCUA(uaObj,'arrActVel')';
+%             actCur = read_OPCUA(uaObj,'arrActCur')';
+            t = (0.001:0.001:7);
+            r= 10.*(t>2)+30-10.*(t>5);
         else
             disp(Pm)
-            actPos=-1.*ones(7000,1);
-            actVel=-1.*ones(7000,1);
-            actCur=-1.*ones(7000,1);
+%             actPos=-1.*ones(7000,1);
+%             actVel=-1.*ones(7000,1);
+%             actCur=-1.*ones(7000,1);
             r=-1.*ones(1,7000);
             t=-1.*ones(1,7000);
         end
 
-        actPos_all=[actPos_all,actPos];
-        actVel_all=[actVel_all,actVel];
-        actCur_all=[actCur_all,actCur];
+%         actPos_all=[actPos_all,actPos];
+%         actVel_all=[actVel_all,actVel];
+%         actCur_all=[actCur_all,actCur];
         r_all=[r_all,r'];
         t_all=[t_all,t'];
-        exp_data.actPos_all=actPos_all;
-        exp_data.actVel_all=actVel_all;
-        exp_data.actCur_all=actCur_all;
+%         exp_data.actPos_all=actPos_all;
+%         exp_data.actVel_all=actVel_all;
+%         exp_data.actCur_all=actCur_all;
         exp_data.r_all=r_all;
         exp_data.t_all=t_all;
-        save('C:\Users\nobar\data_driven_controller\linear_motor\exp_data.mat','exp_data')
+        save('/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/exp_data_33.mat','exp_data')
     end
 end
 %%
@@ -116,8 +119,8 @@ pause(1);
 
 P_all=[];
 D_all=[];
-for D=10:1:90
-    for P=500:100:18000
+for D=10:10:90
+    for P=500:500:12000
         P_all=[P_all,P];
         D_all=[D_all,D];
     end
@@ -125,4 +128,6 @@ end
 load('/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/exp_data_feasible.mat','exp_data')
 exp_data.P=P_all;
 exp_data.D=D_all;
-save('/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/exp_data_feasible_2.mat','exp_data')
+save('/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/exp_data_feasible_3.mat','exp_data')
+
+
--- LM_plots.m
+++ LM_plots.m
@@ -29,7 +29,6 @@ y_offset=30.0300;
 u_offset=-0.2100;
 sampleTs=0.001;
 step_high=40;
-
 for exper=2:200
     % y_offset=exp_data.actPos_all(2000,exper);
     % u_offset=exp_data.actCur_all(2000,exper);
@@ -86,3 +85,86 @@ figure(3)
 hold on
 plot(exp_data.t_all(:,1),exp_data.actCur_all);
 xlabel('Time [s]'); ylabel('Cur')
+%% plot cost in feasible set
+clc;
+clear all;
+close all;
+load('/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/exp_data_feasible.mat','exp_data')
+perf_Data_feasible=[];
+P_feasible=[];
+D_feasible=[];
+P_infeasible=[];
+D_infeasible=[];
+% y_offset=30.0300;
+% u_offset=-0.2100;
+sampleTs=0.001;
+step_high=40;
+for exper=1:length(exp_data.P)
+    exper
+    if exp_data.t_all(1,exper)~=-1
+        sample_idx=exp_data.r_all(:,1)==step_high; %LV sampling time=10 ms
+        tmp_idx=find(sample_idx>0);
+        tmp_idx_2=find(tmp_idx>200); %checkpoint because we know step_up applies no sooner than 0.2 seconds
+        tmp_idx=tmp_idx(tmp_idx_2);
+        y_offset=exp_data.actPos_all(tmp_idx(1)-10,exper);
+        u_offset=exp_data.actCur_all(tmp_idx(1)-10,exper);
+        ytmp = exp_data.actPos_all((tmp_idx(1)-10):tmp_idx(end),exper)-y_offset;
+        utmp = exp_data.actCur_all((tmp_idx(1)-10):tmp_idx(end),exper)-u_offset;
+        %     if exist('G2data')
+        %         G2data = merge(G2data, iddata(ytmp,utmp,sampleTs));
+        %     else
+        %         G2data = iddata(ytmp,utmp,sampleTs);
+        %     end
+        % G2data = merge(G2data, iddata(ytmp,utmp,sampleTs));
+        %calculate performance data based on experimental step response measurements
+        reference0=0;
+        reference=10;
+        y_high=ytmp(10:end);
+        t_high=0:sampleTs:((length(y_high)-1)*sampleTs);
+        S = lsiminfo(y_high,t_high,reference,reference0,'SettlingTimeThreshold',0.02);
+        st=S.SettlingTime;
+        if isnan(st)
+            st=3;
+        end
+        ov=max(0,(S.Max-reference0)/(reference-reference0)-1);
+        Tr=t_high(find(y_high>0.6*(reference-reference0),1))-t_high(find(y_high>0.1*(reference-reference0),1));
+        e=abs(y_high-reference);
+        ITAE = trapz(t_high(1:ceil(5*Tr*1000)), t_high(1:ceil(5*Tr*1000))'.*abs(e(1:ceil(5*Tr*1000))));
+        perf_Data=[ov,Tr,st,ITAE];
+        perf_Data_feasible=[perf_Data_feasible;perf_Data];
+        P_feasible=[P_feasible;exp_data.P(exper)];
+        D_feasible=[D_feasible;exp_data.D(exper)];
+    else
+        P_infeasible=[P_infeasible;exp_data.P(exper)];
+        D_infeasible=[D_infeasible;exp_data.D(exper)];
+    end
+end
+figure(5)
+subplot(4,1,1)
+plot(perf_Data_feasible(:,1))
+ylabel('overshoot')
+subplot(4,1,2)
+plot(perf_Data_feasible(:,2))
+ylim([0,0.02])
+ylabel('rise time')
+subplot(4,1,3)
+plot(perf_Data_feasible(:,3))
+ylabel('settling time')
+subplot(4,1,4)
+plot(perf_Data_feasible(:,4))
+ylabel('ITAE')
+save("/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/Data_feasible.mat","perf_Data_feasible","P_feasible","D_feasible")
+save("/home/mahdi/ETHZ/GBO/code/data_driven_controller/linear_motor/Data_infeasible.mat","P_infeasible","D_infeasible")
+%%
+close
+figure(1)
+hold on
+xlabel("P")
+ylabel("D")
+% scatter(exp_data.P,exp_data.D,"filled","k")
+h_infeasible=scatter(P_infeasible,D_infeasible,"filled","r");
+h_feasible=scatter(P_feasible,D_feasible,"filled","g");
+legend([h_feasible,h_infeasible],{"feasible","infeasible (PM<20)"})
+xlim([500,12000])
+ylim([10,90])
+xticks([500, 2000:2000:12000])
