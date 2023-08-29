a854d4c5b133c1c5f1f12dea1f78a9c5d3a2ee8b
mahdinobar <nobar.mahdi@gmail.com>
2023-08-29 13:02:49 +0200

    metrics plot modified

--- plot_metrics.m
+++ plot_metrics.m
@@ -1,6 +1,6 @@
 close all; clear;clc
 fig=figure(1);
-fig.Position=[200 0 1600 1000];
+fig.Position=[200 0 1600 700];
 ax1=axes;
 ax1.FontSize=24;
 ax1.FontName='Times New Roman';
@@ -25,3 +25,32 @@ set(gca,'ytick',[])
 % ylabel("y")
 % xlabel("time(t)")
 saveas(gca,"/home/mahdi/ETHZ/GBO/code/data_driven_controller/tmp/metrics_visualization_2.png")
+%%
+
+fig=figure(2);
+fig.Position=[200 0 1600 300];
+ax2=axes;
+ax2.FontSize=24;
+ax2.FontName='Times New Roman';
+hold on
+sys = tf(4,[1 2 10]);
+[y,tOut] = step(sys);
+yy=[zeros(21,1);y+0.1-0.501];
+t=linspace(-20,125,length(yy));
+h1=plot(t,yy,"-k","LineWidth",1.5);
+xlim([-20,125])
+ylim([-0.41,0.2])
+%
+yt=[zeros(20,1);tOut.*(y+0.1-0.501)];
+t=linspace(-20,125,length(yt));
+h2=plot(t,yt,"-r","LineWidth",1.5);
+yline(0,"--");
+rlim_up=(0.401+0.1)*102/100;
+rlim_down=(0.401+0.1)*98/100;
+% h3=yline(rlim_up,'--k',"LineWidth",1);
+% h4=yline(rlim_down,'--k',"LineWidth",1);
+legend([h1, h2],["$|y(t)-y_{2}|$","$t|y(t)-y_{2}|$"],'Interpreter','latex',"Location","northeast")
+box on
+set(gca,'xtick',[])
+set(gca,'ytick',[])
+
